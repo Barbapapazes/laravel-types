0879d1261a22ad2577a975e9b94f8c2c3a7cd3be
Barbapapazes <e.soubiran25@gmail.com>
2025-04-24 13:32:48 +0200

    feat: export paginated resource collection links and meta

--- src/index.ts
+++ src/index.ts
@@ -6,30 +6,33 @@ export interface ResourceCollection<T> {
   data: T[]
 }
 
+export interface PaginatedResourceCollectionLinks {
+  first: string
+  last: string
+  next?: string
+  prev?: string
+}
+
+export interface PaginatedResourceCollectionMeta {
+  current_page: number
+  from?: number
+  last_page: number
+  links: {
+    active: boolean
+    label: string
+    url?: string
+  }[]
+  path: string
+  per_page: number
+  to?: number
+  total: number
+}
+
 export interface PaginatedResourceCollection<T> {
   data: T[]
 
-  links: {
-    first: string
-    last: string
-    next?: string
-    prev?: string
-  }
-
-  meta: {
-    current_page: number
-    from?: number
-    last_page: number
-    links: {
-      active: boolean
-      label: string
-      url?: string
-    }[]
-    path: string
-    per_page: number
-    to?: number
-    total: number
-  }
+  links: PaginatedResourceCollectionLinks
+  meta: PaginatedResourceCollectionMeta
 };
 
 export default {}
